92b3d80d7c92b01a47e2b5a5fe5684842b760310
Alberto <alberto.alvarez.sanz@gmail.com>
2020-11-08 11:13:09 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        /*
+         Implementa un programa en java que use hilos independientes, en una misma clase y
+          que imprima los pares y los impares del 1 al 10 inclusive.
+         */
+
+        /*
+        Despues de crear la clase hilos, especificamos un objeto  -> linea 16
+        una vez definido el objeto lo iniciamos con start()       -> linea 17
+         */
+
+        hilosImpares h1 = new hilosImpares();
+        h1.start();
+
+
+    }
+
+}
---
+++ src/hilosImpares.java
@@ -0,0 +1,18 @@
+
+
+/*  Instrucciones para generar hilos
+    1º hacer en el extends      -> linea 9
+    2º poner run y luego BORRAR el super, se generara un override 11 - 17
+
+ */
+
+public class hilosImpares extends Thread{
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++){
+            if (i % 2 != 0)
+            System.out.println(i);
+        }
+    }
+}
